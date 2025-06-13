92c21e06ddf8c95693d47c3a7c2d7863d84054bb
Angel castañeda <luis.castaneda@noktos.com>
2025-06-13 11:27:33 -0600

    ADD solicitud bien

--- api/v1/router/mia/solicitud.js
+++ api/v1/router/mia/solicitud.js
@@ -1,16 +1,21 @@
-const controller = require("../../controller/solicitud")
-const router = require("express").Router()
-const middleware = require("../../middleware/validateParams")
+const controller = require("../../controller/solicitud");
+const router = require("express").Router();
+const middleware = require("../../middleware/validateParams");
 
-const requiredParamsToCreate = ["solicitudes"]
-router.post("/", middleware.validateParams(requiredParamsToCreate), controller.create)
-router.get("/", controller.read)
-router.get("/client", controller.readClient)
-router.get("/id", controller.readSolicitudById)
-router.get("/viajero", controller.getViajeroFromSolicitud)
-router.get("/viajeroAgente", controller.getViajeroAgenteFromSolicitud)
-router.get("/withviajero", controller.readSolicitudByIdWithViajero)
-router.get("/consultas", controller.readConsultas)
-router.get("/items", controller.getItemsSolicitud)
+const requiredParamsToCreate = ["solicitudes"];
+router.post(
+  "/",
+  middleware.validateParams(requiredParamsToCreate),
+  controller.create
+);
+router.get("/", controller.read);
+router.get("/client", controller.readClient);
+router.get("/id", controller.readSolicitudById);
+router.get("/viajero", controller.getViajeroFromSolicitud);
+router.get("/viajeroAgente", controller.getViajeroAgenteFromSolicitud);
+router.get("/withviajero", controller.readSolicitudByIdWithViajero);
+router.get("/consultas", controller.readConsultas);
+router.get("/items", controller.getItemsSolicitud);
+router.get("/forclient", controller.readForClient);
 
-module.exports = router
+module.exports = router;
