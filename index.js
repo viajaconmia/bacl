48d962ae154fa15b14aee25c170314d30966a5be
Angel castañeda <luis.castaneda@noktos.com>
2025-07-16 14:09:18 -0600

    ADD CORS to pruebas

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const corsOptions = {
     "https://mia-gray.vercel.app",
     "https://www.viajaconmia.com",
     "https://admin.viajaconmia.com",
+    "https://mia-git-pruebasmia-mias-projects-f396ca8b.vercel.app",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: [
