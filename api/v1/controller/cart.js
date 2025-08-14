0a889ebf18051a13d39d35d1be8e7187fe72492c
Angel Castañeda <luisacast.29@gmail.com>
2025-08-14 15:45:22 -0600

    Manejo de carrito

--- api/v1/controller/cart.js
+++ api/v1/controller/cart.js
@@ -1,6 +1,7 @@
 const { executeQuery } = require("../../../config/db");
 const { CustomError } = require("../../../middleware/errorHandler");
 
+/****************+ GET ********************* */
 const getCartItemsById = async (req, res) => {
   try {
     const { id_agente, id_viajero } = req.query;
@@ -12,11 +13,159 @@ const getCartItemsById = async (req, res) => {
         null
       );
 
-    const cartItems = executeQuery(`SELECT * FROM `);
+    const cartItemsSolicitudes = await executeQuery(
+      `select 
+  c.*, 
+  s.id_servicio, 
+  s.id_viajero, 
+  s.hotel, 
+  s.check_in, 
+  s.check_out, 
+  s.room,
+  s.id_hotel,
+  s.id_acompanantes 
+  from cart c 
+left join solicitudes s on s.id_solicitud = c.id_solicitud
+where (c.id_agente = ? OR c.usuario_generador = ?) AND c.active = 1;`,
+      [id_agente || "", id_viajero || ""]
+    );
+
+    const carItems = cartItemsSolicitudes.map((item) => ({
+      id: item.id,
+      total: Number(item.total),
+      type: item.type,
+      selected: Boolean(item.selected),
+      details: {
+        hotel: item.hotel,
+        id_hotel: item.id_hotel,
+        id_agente: item.id_agente,
+        check_in: item.check_in,
+        check_out: item.check_out,
+        room: item.room,
+        viajero_principal: item.id_viajero,
+        id_acompanantes: item.id_acompanantes,
+        noches: 0,
+        usuario_creador: item.usuario_generador,
+        active: item.active,
+        id_servicio: item.id_servicio,
+        id_solicitud: item.id_solicitud,
+        created_at: item.created_at,
+        updated_at: item.updated_at,
+      },
+    }));
 
     res.status(200).json({
       message: "Obtenidos con exito",
-      data: { id_agente, id_viajero },
+      data: carItems,
+    });
+  } catch (error) {
+    console.log("Este es el error qiuien c", error.message);
+    return res.status(error.statusCode || 500).json({
+      error: error.details,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+/****************+ DELETE ********************* */
+const deleteCartItem = async (req, res) => {
+  try {
+    const { id } = req.query;
+    if (!id)
+      throw new CustomError(
+        "Falta el id del item del carrito",
+        400,
+        "MISSING_ITEM_ID",
+        null
+      );
+    await executeQuery(`update cart set active = 0 where id = ?;`, [id]);
+
+    res.status(204).json({
+      message: "Item eliminado con exito",
+      data: null,
+    });
+  } catch (error) {
+    console.log("Este es el error qiuien c", error.message);
+    return res.status(error.statusCode || 500).json({
+      error: error.details,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+/****************+ POST ********************* */
+const createCartItem = async (req, res) => {
+  try {
+    const {
+      id_solicitud,
+      id_agente,
+      usuario_generador,
+      total,
+      type,
+      selected,
+    } = req.body;
+    if (
+      !id_solicitud ||
+      !id_agente ||
+      !usuario_generador ||
+      !total ||
+      !type ||
+      selected === undefined
+    )
+      throw new CustomError(
+        "Faltan datos necesarios para crear el item del carrito",
+        400,
+        "MISSING_DATA",
+        null
+      );
+
+    const result = await executeQuery(
+      `INSERT INTO cart (
+    total,
+    id_agente,
+    type,
+    selected,
+    id_solicitud,
+    usuario_generador
+) VALUES ( ?, ?, ?, ?, ?, ?); `,
+      [total, id_agente, type, selected, id_solicitud, usuario_generador]
+    );
+
+    res.status(201).json({
+      message: "Item creado con exito",
+      data: result,
+    });
+  } catch (error) {
+    console.log("Este es el error qiuien c", error.message);
+    return res.status(error.statusCode || 500).json({
+      error: error.details,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+/****************+ PATCH ********************* */
+const setSelectedCartItem = async (req, res) => {
+  try {
+    const { id, selected } = req.body;
+    if (!id || selected === undefined)
+      throw new CustomError(
+        "Falta el id del item del carrito",
+        400,
+        "MISSING_ITEM_ID",
+        null
+      );
+    await executeQuery(`update cart set selected = ? where id = ?;`, [
+      selected,
+      id,
+    ]);
+
+    res.status(204).json({
+      message: "Item actualizado con exito",
+      data: null,
     });
   } catch (error) {
     console.log("Este es el error qiuien c", error.message);
@@ -30,4 +179,7 @@ const getCartItemsById = async (req, res) => {
 
 module.exports = {
   getCartItemsById,
+  deleteCartItem,
+  setSelectedCartItem,
+  createCartItem,
 };
--- api/v1/router/mia/cart.js
+++ api/v1/router/mia/cart.js
@@ -2,5 +2,8 @@ const controller = require("../../controller/cart");
 const router = require("express").Router();
 
 router.get("/", controller.getCartItemsById);
+router.post("/", controller.createCartItem);
+router.delete("/", controller.deleteCartItem);
+router.patch("/", controller.setSelectedCartItem);
 
 module.exports = router;
