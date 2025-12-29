ef3f4b466eca6ec80351a282f15a33fdc8df84cc
Angel castañeda <luis.castaneda@noktos.com>
2025-12-29 13:05:00 -0600

    done

--- api/v1/router/mia/proveedores.js
+++ api/v1/router/mia/proveedores.js
@@ -1,12 +1,12 @@
 const controller = require("../../controller/proveedores");
 const router = require("express").Router();
 
-router.get("/", controller.getProveedores);
+router.get("/", controller.getProveedores); // obtiene una lista de proveedores
+router.get("/detalles", controller.getDetalles); // obtiene los datos fiscales del proveedor
 router.post("/", controller.createProveedor);
 router.post("/sucursal", controller.crearSucursal);
 router.get("/sucursal", controller.getSucursales);
-router.get("/detalles",controller.getDetalles);
-router.put("editar_proveedor",controller.putEditar);
-router.put("editar_cuenta",controller.putEditarCuenta);
+router.put("editar_proveedor", controller.putEditar);
+router.put("editar_cuenta", controller.putEditarCuenta);
 
 module.exports = router;
