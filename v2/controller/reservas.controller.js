8e9a2588eb54d309aeaaf2819097b8d0b05a3b95
Angel castañeda <luis.castaneda@noktos.com>
2026-02-20 11:37:28 -0600

    done

--- v2/controller/reservas.controller.js
+++ v2/controller/reservas.controller.js
@@ -1997,7 +1997,7 @@ const obtener = async (req, res) => {
   /* =========================
      FILTROS
   ==========================*/
-  where.push(`admin_creador <> ?`);
+  where.push(`admin_creador <> ? OR is null`);
   params.push("cef88247-b690-11f0-9e79-06cc8e8ac9fd");
 
   // Código reservación
